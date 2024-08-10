761112b8b536f412ef39f88151c0811499c59e1f
satyam <satyamkale271@gmail.com>
2024-08-10 21:17:28 +0530

    time required to  handel request consol.log

--- src/routers/courses.controllers.js
+++ src/routers/courses.controllers.js
@@ -1,6 +1,9 @@
 const coursesData = require("../models/courses.model");
 function getcourses(req, res) {
+  const start = Date.now();
   res.status(200).json(coursesData);
+  const delta = Date.now() - start;
+  console.log(`${delta}ms`);
 }
 module.exports = {
   getcourses,
