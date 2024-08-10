b249a2df42d191e2358d859b542acc4eaa4ec3b3
satyam <satyamkale271@gmail.com>
2024-08-10 18:17:50 +0530

    data in models

---
+++ src/models/courses.model.js
@@ -0,0 +1,15 @@
+const courses = [
+  {
+    id: 0,
+    course_name: "Begineer guide to javascript",
+  },
+  {
+    id: 1,
+    course_name: "react begineer to advance",
+  },
+  {
+    id: 2,
+    course_name: "Nodejs complete guide",
+  },
+];
+module.exports = courses;
